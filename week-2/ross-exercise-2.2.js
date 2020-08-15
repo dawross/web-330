fae5ffdf084c59d73e0a6fcb40c4ea33ec20aeeb
Dan Ross <dawross@my365.bellevue.edu>
2020-08-15 15:54:35 -0500

    exercise 2.2

---
+++ week-2/ross-exercise-2.2.js
@@ -0,0 +1,43 @@
+/*
+============================================
+; Title: Exercise 2.2
+; Author: Dan Ross
+; Date: 15 August 2020
+; Description: Prototypes
+;===========================================
+*/
+//Imports the ross-header.js file from the root directory
+const header = require("../ross-header.js");
+//person object
+var person = {
+    getAge: function() {
+        //return age
+        return this.age;
+    }
+};
+// create new person object with age and full name
+var dan = Object.create(person, {
+    age:
+    {
+        "value": "46"
+    },
+    "fullname":
+    {
+        "value": "Dan Ross"
+    }
+});
+
+//call getAge function on 'dan' person object
+dan.getAge();
+
+
+// output header
+console.log(header.display("Dan", "Ross", "Exercise 2.2"));
+console.log("");
+
+//output
+console.log("The person's full name is " + dan.fullname + ".");
+console.log("The person's age is " + dan.age + ".");
+
+
+
